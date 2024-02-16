a9c88bb6952ad65e0268af6c91904d857e25f7f4
Artur Leshchenko <artur151199@mail.ru>
2024-02-16 18:04:20 +0300

    Person

--- src/Service/PersonService.java
+++ src/Service/PersonService.java
@@ -28,33 +28,53 @@ public class PersonService {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите возраст для фильтрации");
         String age1 =  sc.nextLine();
-        int age = Integer.parseInt (age1);
+       // int age = Integer.parseInt (age1);
 
 
 
         List <Person> filtredList1 = new ArrayList<>();
 
-
-        for (Person p : persons) {
-            if (p.getAge() == age) {
-                filtredList1.add(p);
-            }
-            if (age1.contains("-")) {
-                int znak;
-                znak = age1.indexOf('-');
-                String z;
-                z = age1.substring(0, znak);
-                int zz = Integer.parseInt(z);
-                String y;
-                y = age1.substring(znak + 1);
-                int yy = Integer.parseInt(y);
+        if (age1.contains("-")) {
+            int znak = age1.indexOf('-');
+            String z = age1.substring(0, znak);
+            int zz = Integer.parseInt(z);
+            String y = age1.substring(znak + 1);
+            int yy = Integer.parseInt(y);
+            for (Person p : persons) {
                 if ((p.getAge() > zz) && (p.getAge() < yy)) {
                     filtredList1.add(p);
                 }
             }
         }
+        else
+                for (Person p : persons) {
+                    int age = Integer.parseInt (age1);
+                    if (p.getAge() == age) {
+                        filtredList1.add(p);
+                    }
+                }
+
+
+//        for (Person p : persons) {
+//            if (p.getAge() == age) {
+//                filtredList1.add(p);
+//            }
+//            if (age1.contains("-")) {
+//                int znak;
+//                znak = age1.indexOf('-');
+//                String z;
+//                z = age1.substring(0, znak);
+//                int zz = Integer.parseInt(z);
+//                String y;
+//                y = age1.substring(znak + 1);
+//                int yy = Integer.parseInt(y);
+//                if ((p.getAge() > zz) && (p.getAge() < yy)) {
+//                    filtredList1.add(p);
+//                }
+//            }
+
         if (filtredList1.isEmpty()) {
-            System.out.println("Возраст персоны " + age + " не найден");
+            System.out.println("Возраст персоны " + age1 + " не найден");
             return filtredList1;
         }
 
